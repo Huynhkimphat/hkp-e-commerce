7db5bb48b0045e37e22384cd46037ffe016ca650
Khang Huynh <hgiakhang20@gmail.com>
2025-07-28 21:13:49 +0700

    feat: init prj

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+// page.tsx
+const Homepage = () => {
+  return (
+    // Thêm các lớp trực tiếp để kiểm tra biến thể lg:
+    <div>
+      Prostore
+    </div>
+  );
+}
+
+export default Homepage;
